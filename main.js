538323776f76812123deb98e104150ef8a0a82f2
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-04-28 14:54:28 -0500

    Update version format in main.js

--- main.js
+++ main.js
@@ -372,7 +372,7 @@ userDevicesCache: userDevicesCache || new Map(),
 //msgRetryCounterMap,
 defaultQueryTimeoutMs: undefined,
 cachedGroupMetadata: (jid) => global.conn.chats[jid] ?? {},
-version: version, 
+version: [2, 3000, 1033959288],
 keepAliveIntervalMs: 55000, 
 maxIdleTimeMs: 60000, 
 };
