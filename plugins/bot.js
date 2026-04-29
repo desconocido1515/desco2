d569a27ec5cfe3e8ac2a50161a32ff0362de9120
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-04-29 07:11:35 -0500

    Change trigger word from 'bott' to 'bot5'

--- plugins/bot.js
+++ plugins/bot.js
@@ -3,7 +3,7 @@ export async function before(m, { conn }) {
   if (!m.text) return
   if (m.isBaileys) return
 
-  if (m.text.trim().toLowerCase() === 'bott') {
+  if (m.text.trim().toLowerCase() === 'bot5') {
 
     await conn.reply(
       m.chat,
