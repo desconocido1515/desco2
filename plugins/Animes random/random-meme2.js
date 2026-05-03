017538c06d4b38b836ff563be4f9d1a62ba4ac1e
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-05-03 12:44:40 -0500

    Delete plugins directory

--- plugins/Animes random/random-meme2.js
+++
@@ -1,9 +0,0 @@
-import hispamemes from 'hispamemes'
-let handler = async (m, { conn, usedPrefix, command }) => {
-const meme = hispamemes.meme()
-conn.sendFile(m.chat, meme, 'error.jpg', `😂😂🤣`, m)
-//await conn.sendButton(m.chat, `*_${command}_*`.trim(), wm, meme, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
-}
-handler.command = ['meme2', 'memes2'] 
-handler.level = 0
-export default handler
--- plugins/Propietario/propietario(a)-leavegc.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, text, command }) => {
-let id = text ? text : m.chat  
-await conn.reply(id, `*${wm} 𝘼𝘽𝘼𝙉𝘿𝙊𝙉𝘼 𝙀𝙇 𝙂𝙍𝙐𝙋𝙊, 𝙁𝙐𝙀 𝙂𝙀𝙉𝙄𝘼𝙇 𝙀𝙎𝙏𝘼𝙍 𝘼𝙌𝙐𝙄 👋*`) 
-await conn.groupLeave(id)}
-handler.command = /^(salir|leavegc|leave)$/i
-handler.group = true
-handler.rowner = true
-export default handler
-
